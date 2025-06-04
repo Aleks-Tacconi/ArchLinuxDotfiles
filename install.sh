3d5e2fd10a66873903f03c6f95dce70c34141020
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-06-04 09:37:18 +0000

    change install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ sudo pacman -S --needed base-devel git
 
 if ! command -v yay &> /dev/null; then
     git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+    cd ../
 fi
 
-cd ../
 
 terminal=(
     "kitty"                       # Terminal emulator
